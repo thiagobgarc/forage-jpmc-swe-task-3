41c0c1734a2c129e38517a1e6347f6b8e9fc71c6
thiagobgarc <thiagobuenogarcia1@gmail.com>
2023-09-03 13:19:11 -0400

    forage-jpmc-swe-task-3

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,53 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  // The price of abc
+  price_abc: number,
+  // The price of def
+  price_def: number,
+  // The ratio of price_abc to price_def
+  ratio: number,
+  // The timestamp of data
   timestamp: Date,
+  // The upper bound value
+  upper_bound: number,
+  // The lower bound value
+  lower_bound: number,
+  // The trigger alert value, which is optional and can be undefined
+  trigger_alert: number | undefined, 
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  static generateRow(serverResponds: ServerRespond[]): Row {
+    // Calculate the average price of ABC by adding the top ask and top bid prices and dividing by 2
+    const priceABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2;
+  
+    // Calculate the average price of DEF by adding the top ask and top bid prices and dividing by 2
+    const priceDEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
+  
+    // Calculate the ratio of priceABC to priceDEF
+    const ratio = priceABC / priceDEF;
+  
+    // Set the upper bound value to 1 + 0.05
+    const upperBound = 1 + 0.05;
+  
+    // Set the lower bound value to 1 - 0.05
+    const lowerBound = 1 - 0.05;
+    return {
+      price_abc: priceABC, // Set the value of price_abc property to the variable priceABC
+      price_def: priceDEF, // Set the value of price_def property to the variable priceDEF
+      ratio, // Set the value of ratio property to the variable ratio
+      timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ? 
+        serverResponds[0].timestamp : serverResponds[1].timestamp,
+        // Set the value of timestamp property to the larger timestamp value between serverResponds[0] and serverResponds[1]
+    
+      upper_bound: upperBound, // Set the value of upper_bound property to the variable upperBound
+      lower_bound: lowerBound, // Set the value of lower_bound property to the variable lowerBound
+    
+      trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
+        // Set the value of trigger_alert property to ratio if it is outside the bounds, otherwise set it to undefined
+    }
+    }
   }
-}
+
--- src/Graph.tsx
+++ src/Graph.tsx
@@ -23,35 +23,50 @@ class Graph extends Component<IProps, {}> {
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
+      price_abc: 'float',
+      price_def: 'float',
+      ratio: 'float',
       timestamp: 'date',
+      upper_bound: 'float',
+      lower_bound: 'float',
+      trigger_alert: 'float',
     };
 
     if (window.perspective && window.perspective.worker()) {
       this.table = window.perspective.worker().table(schema);
     }
+    // Check if the table exists
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
+      // Set the view type to 'y_line'
       elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
+      // Set the row pivots to '["timestamp"]'
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      // Set the columns to '["ratio", "lower_bound", "upper_bound", "trigger_alert"]'
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]');
+       // Set the aggregates using the provided JSON string representation
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
+        price_abc: 'avg',
+        price_def: 'avg',
+        ratio: 'avg',
         timestamp: 'distinct count',
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg',
       }));
     }
   }
 
   componentDidUpdate() {
+    // Check if the table exists
     if (this.table) {
       this.table.update(
-        DataManipulator.generateRow(this.props.data),
+        [
+          // Generate a row using the props data
+          DataManipulator.generateRow(this.props.data),
+          // Update the table with the generated row
+        ] as unknown as TableData
       );
     }
   }
